d38a6652fe63fed1bbe559b81e5451e8ed0ba610
Shyam Sunder Grover <post2shyam@gmail.com>
2021-02-02 19:26:45 +0530

    Created Skeleton of Home Page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    (<div className="header"> Phone Directory </div>),
+    (<button> Add </button>),
+    (
+      <div>
+        <span> Name </span> <span> Phone </span>{" "}
+      </div>
+    )
+  );
+}
+
+export default App;
