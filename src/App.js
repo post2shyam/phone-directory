cb579fe0aaab11ca1afb41c2d426689144551b42
Shyam Grover <post2shyam@gmail.com>
2021-02-04 19:20:08 +0530

    Addes css for app component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Header from "./Header";
 function App() {
   return (
     <div>
-      <Header/>
-      <button> Add </button>
-      <div>
-        <span> Name </span> <span> Phone </span>{" "}
+      <Header />
+      <div className="component-body-container">
+        <button className="custom-btn add-btn">Add</button>
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span>
+          <span className="grid-item phone-heading">Phone</span>
+        </div>
       </div>
     </div>
   );
